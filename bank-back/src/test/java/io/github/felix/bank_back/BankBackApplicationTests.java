d2e68c25687c266417bc6a7a8cd7e14bb40e3190
FelixRodriguezG <correofrg@gmail.com>
2025-09-05 14:08:35 +0200

    initial commit

---
+++ bank-back/src/test/java/io/github/felix/bank_back/BankBackApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.felix.bank_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
